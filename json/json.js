dccf0f6634497775a906436eaac93b8f7cc59918
Scott Pinkelman <scott@scottpinkelman.com>
2016-06-11 16:12:16 -0400

    adds test file for json integration

--- json/json.js
+++ json/json.js
@@ -1,9 +1,10 @@
 /* jshint devel: true */
 /*global  stationInformation, stationStatus, srcGeoJSON */
+(function() {
+'use strict';
 
 
 function combine () {
-  'use strict';
   
   var geoJSON = {
     'features' : [], 
@@ -16,11 +17,12 @@ function combine () {
   console.log(geoJSON);
 
   stationInformation = stationInformation.data.stations;
-
+  stationStatus = stationStatus.data.stations;
+  console.log(stationStatus);
+  //stationStatus = station_status.data
 
   
   for (var i = 0; i < stationInformation.length; ++i) {
-    console.log(stationInformation[i]);
     var geoObj = {
       'geometry' : {
         'coordinates' : [stationInformation[i].lat, stationInformation[i].lon],
@@ -33,8 +35,15 @@ function combine () {
 
       },
       'type' : 'feature'
-
     };
+    for (var j = 0; j < stationStatus.length; ++j) {
+      if (stationStatus[j].station_id === geoObj.properties.station_id) {
+        geoObj.properties.last_reported = stationStatus[j].last_reported;
+        geoObj.properties.num_bikes_available = stationStatus[j].num_bikes_available;
+        geoObj.properties.num_docks_available = stationStatus[j].num_docks_available;
+        break;
+      }
+    }
     //localGeoObj.geometry.coordinates = [1,2];
 
     geoJSON.features.push(geoObj); 
@@ -44,4 +53,7 @@ function combine () {
   console.log(geoJSON);
 }
 
+
 combine();
+
+})();
