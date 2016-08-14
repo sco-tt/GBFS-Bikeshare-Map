9d1e4590c346b5e1276336a421489642d9d41bed
Scott Pinkelman <scott@scottpinkelman.com>
2016-08-13 20:38:24 -0600

    Adds serve command

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,18 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 
-
 // include plug-ins
 var jshint = require('gulp-jshint');
 
 
-// Sub tasks
-gulp.task('jshint', function() {
+// JS Sub tasks
+gulp.task('_jshint', function() {
   gulp.src('./src/js/app.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('browserify', function() {
+gulp.task('_browserify', function() {
     return browserify('./src/js/app.js')
         .bundle()
         //Pass desired output filename to vinyl-source-stream
@@ -31,7 +30,7 @@ gulp.task('browserify', function() {
 });
 
 // JS tasks
-gulp.task('js',['jshint','browserify'], function () {
+gulp.task('js',['_jshint','_browserify'], function () {
    gulp.src('./build/assets/js/app.js')
       .pipe(uglify())
       .pipe(rename('app.min.js'))
@@ -39,27 +38,30 @@ gulp.task('js',['jshint','browserify'], function () {
 });
 
 // CSS task
-
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./build/assets/css'));
+    .pipe(gulp.dest('./build/assets/css'))
+    .pipe(browserSync.stream());
 });
 
 // HTML TAsk
-
 gulp.task('html', function() {
   return gulp.src('./src/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./build'));
+    browserSync.reload;
 });
 
 
-// Watch
-gulp.task('serve', function() {
-    gulp.watch('./src/js/*.js', ['js']);
+// Serve 
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./build"
+    });
     gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/*.html', ['html']).on('change', browserSync.reload);
 });
 
 
